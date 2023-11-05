8457f7aa5e63dbd0206dfcb2207b5ea854c4f665
cylorun <allidabbi@gmail.com>
2023-11-05 20:21:47 +0000

    blind and sh splits

--- stats.py
+++ stats.py
@@ -1,12 +1,18 @@
 import glob, os, json, time, tkinter, threading
+# add break threashold
 
 def make_stats():
     enter_times = []
     bastion_times = []
     fort_times = []
+    blind_times = []
+    stronghold_times = []
     enters = 0
     bastions = 0
     forts = 0
+    blinds = 0
+    strongholds = 0
+    delete_all_records()
     print(f'Tracking {records_path}')
     while True:
         records = glob.glob(f'{records_path}\\*.json')
@@ -16,7 +22,6 @@ def make_stats():
 
             timelines = data.get("timelines", [])
             for timeline in timelines:
-                print('broom')
                 if timeline.get("name") == 'enter_nether':
                     enters += 1
                     enter_times.append(timeline.get("igt", 0) / 1000)
@@ -26,7 +31,13 @@ def make_stats():
                 if timeline.get("name") == 'enter_fortress':
                     forts += 1
                     fort_times.append(timeline.get("igt", 0) / 1000)
-                os.remove(record)
+                if timeline.get("name") == 'nether_travel':
+                    blinds += 1
+                    blind_times.append(timeline.get("igt", 0) / 1000)
+                if timeline.get("name") == 'enter_stronghold':
+                    strongholds += 1
+                    stronghold_times.append(timeline.get("igt", 0) / 1000)
+                # os.remove(record)
                 
         with open(f'{os.getcwd()}\\stats.txt', 'w+') as stat_file:
             stats = ''
@@ -40,8 +51,16 @@ def make_stats():
                         case 'bastion-time': stats+= f'BATSION ENTER: {avg_time(bastion_times)}\n'
                         case 'fort-count': stats+= f'FORTS: {forts}\n'
                         case 'fort-time': stats+= f'FORT ENTER: {avg_time(fort_times)}\n'
+                        case 'blind-count': stats+= f'BLINDS: {blinds}\n'
+                        case 'blind-time': stats+= f'AVG BLIND: {avg_time(blind_times)}\n'
+                        case 'stronghold-count': stats+= f'STRONGHOLDS: {strongholds}\n'
+                        case 'stronghold-time': stats+= f'AVG STRONGHOLD: {avg_time(stronghold_times)}\n'
             stat_file.write(stats)
         time.sleep(10)
+def delete_all_records():
+    print(str(*[os.remove(json_file) for json_file in glob.glob(f'{records_path}/*.json')]).replace('None',''),'Deleting records')
+
+
 
 def avg_time(times):
     min, sec = divmod(int(sum(times) // len(times) if times else 0), 60)
@@ -82,15 +101,19 @@ settings = {
     "bastion-time":True,
     "bastion-count":True,
     "fort-time":True,
-    "fort-count":True
+    "fort-count":True,
+    "blind-time":True,
+    "blind-count":True,
+    "stronghold-time":True,
+    "stronghold-count":True
 }
 
 load_settings()
 for i,key in enumerate(settings):
     make_box(i,key)
 
-tkinter.Button(root, text="Save", command=save_settings).grid(row=8,column=0)
+tkinter.Button(root, text="Save", command=save_settings).grid(row=12,column=0)
 threading.Thread(target=make_stats).start()
 root.resizable(False,False)
-root.geometry('150x230')
+root.geometry('150x330')
 root.mainloop()
